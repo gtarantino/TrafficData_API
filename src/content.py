8511fa0af60fa52c935205afb389f37806654c87
Giancarlo Tarantino <gtaranti@calpoly.edu>
2017-12-04 20:43:07 -0800

    no works with new data layout

--- src/content.py
+++ src/content.py
@@ -5,21 +5,10 @@ from flask import Blueprint, request
 
 content_api = Blueprint('content_api', __name__)
 
-#@content_api.route("/getcontent/<int:year>/<int:month>/<int:day>")
-#def getTrafficData(year, month, day):
-@content_api.route("/getcontent")
-def getcontent():
+@content_api.route("/getTrafficData")
+def getTrafficData():
    try:
-      f = open('../datasource/10_23_2017_22_08.json', 'r')
-   except IOError:
-      return json.dumps({'message': 'no information for given date'}), 500
-
-   return json.dumps(json.load(f)), 200
-
-@content_api.route("/getsingletrafficobj")
-def getsingletrafficobj():
-   try:
-      f = open('../datasource/testobj.json', 'r')
+      f = open('../datasource/TrafficData.json', 'r')
    except IOError:
       return json.dumps({'message': 'no information for given date'}), 500
 
